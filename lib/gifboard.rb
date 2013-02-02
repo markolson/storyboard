b39164a80e6aa25de171b7c2c80e4a2ec5325867
mark <theothermarkolson@gmail.com>
2013-02-02 11:31:03 -0500

    First draft of Gifboard

---
+++ lib/gifboard.rb
@@ -0,0 +1,101 @@
+require 'storyboard/subtitles.rb'
+require 'storyboard/bincheck.rb'
+require 'storyboard/thread-util.rb'
+require 'storyboard/time.rb'
+require 'storyboard/version.rb'
+require 'storyboard/cache.rb'
+
+require 'storyboard/generators/sub.rb'
+require 'storyboard/generators/gif.rb'
+
+require 'mime/types'
+require 'fileutils'
+require 'tmpdir'
+
+require 'ruby-progressbar'
+require 'mini_magick'
+
+require 'json'
+
+class Gifboard < Storyboard
+  attr_accessor :options, :capture_points, :subtitles, :timings
+  attr_accessor :length, :renderers, :mime, :cache
+
+  def initialize(o)
+    super
+  end
+
+  def run
+    LOG.info("Processing #{options[:file]}")
+    setup
+
+    @cache = Cache.new(Suby::MovieHasher.compute_hash(Path.new(options[:file])))
+
+    LOG.debug(options) if options[:verbose]
+
+    @subtitles = SRT.new(options[:subs] ? File.read(options[:subs]) : get_subtitles, options)
+    # bit of a temp hack so I don't have to wait all the time.
+    @subtitles.save if options[:verbose]
+
+    @cache.save
+
+    if @options[:text]
+      @renderers << Storyboard::GifRenderer.new(self)
+
+      selected = choose_text
+      p selected.end_time.value - selected.start_time.value
+      @capture_points << selected.start_time
+      (0.1).step(selected.end_time.value - selected.start_time.value, 0.1) {|i|
+        @capture_points << selected.start_time + i
+      }
+      # @capture_points << selected.end_time not sure if it's better or worse to leave this yet
+
+      @stop_frame = @capture_points.count
+      extract_frames
+
+      render_output
+    else
+      @subtitles.pages.each {|x|
+        print "[#{x.start_time.to_srt}]\t"
+        x.lines.each_with_index{|l,i|
+          print "\t\t" if i > 0
+          puts l
+        }
+      }
+    end
+  end
+
+  def choose_text
+    matches = []
+    @subtitles.pages.each {|x|
+      found = !x.lines.select {|l|
+        l.downcase.match(options[:text].downcase)
+      }.empty?
+      matches << x if found
+    }
+
+    match = nil
+    if matches.count == 0
+      raise "No matches found"
+    elsif matches.count == 1
+      puts "Just one match found. Using it."
+      match = matches.first
+    else
+      puts "Multiple matches found.. pick one!"
+      matches.each_with_index {|m,i|
+        puts "#{i+1}: [#{m.start_time.to_srt}] #{m.lines.join}"
+      }
+      while !match
+        print "choice (default 1): "
+        input = gets.chomp
+        number = input.empty? ? 1 : input.to_i
+        if number > matches.count || number < 1
+          puts "Try again. Choose a subtitle between 1 and #{matches.count}"
+        else
+          match = matches[number-1]
+        end
+      end
+    end
+    match
+  end
+end
---
+++ lib/storyboard/generators/gif.rb
@@ -0,0 +1,42 @@
+require 'prawn'
+require 'mini_magick'
+
+class Storyboard
+  class GifRenderer < Storyboard::Renderer
+
+    attr_accessor :pdf, :storyboard, :dimensions
+    def initialize(parent)
+      @dimensions = []
+      @storyboard = parent
+    end
+
+    def set_dimensions(w,h)
+      @dimensions = [w,h]
+    end
+
+    def write
+      `cd #{@storyboard.options[:save_directory]} && convert -coalesce -delay 10 -layers OptimizeTransparency -loop 0 -ordered-dither o8x8,8,8,4 +map sub-* "#{@storyboard.options[:write_to]}/#{@storyboard.options[:basename]}.gif"`
+      `open #{@storyboard.options[:write_to]}`
+      LOG.info("Wrote #{@storyboard.options[:write_to]}/#{@storyboard.options[:basename]}.gif")
+    end
+
+    def render_frame(frame_name, subtitle = nil)
+      output_filename = File.join(@storyboard.options[:save_directory], "sub-#{File.basename(frame_name)}")
+      image = MiniMagick::Image.open(frame_name)
+
+      if(@dimensions.empty?)
+        resize_height = (image[:height] * (@storyboard.options[:max_width].to_f/image[:width])).ceil
+        set_dimensions(storyboard.options[:max_width], resize_height)
+      end
+
+      image.resize "#{@dimensions[0]}x#{@dimensions[1]}"
+      image.quality("60")
+
+      self.add_subtitle(image, subtitle, @dimensions) if subtitle
+      image.format 'jpeg'
+      image.write(output_filename)
+      image.destroy!
+    end
+
+  end
+end
